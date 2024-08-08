308c7a56f6502144c5579b53d269bd35b9f4e785
Michael Mwangi <michaelmichire2016@gmail.com>
2024-08-08 07:06:42 -0700

    Task_4

--- 0x02-Session_authentication/api/v1/auth/auth.py
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -1,19 +1,19 @@
 #!/usr/bin/env python3
+
 """
-Module to manage Authentication
+API authentication module
 """
+from os import getenv
 from flask import request
 from typing import List, TypeVar
 
 
 class Auth:
-    """
-    Manage the API authentication
-    """
+    """ Authentication """
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """
-        Check if authentication is required to access path
+        Determines if authentication is required
         """
         if path is None or not excluded_paths:
             return True
@@ -26,7 +26,7 @@ class Auth:
 
     def authorization_header(self, request=None) -> str:
         """
-        Method that handles authorization header
+        Retrieves authorization header
         """
         if request is None:
             return None
@@ -35,7 +35,7 @@ class Auth:
 
     def current_user(self, request=None) -> TypeVar('User'):
         """
-        Validates current user
+        Retrieves the current user
         """
         return None
 
